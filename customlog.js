c53561f0a2d164fbeefb9cfda802b67ebba84c4c
keli5 <46039037+keli5@users.noreply.github.com>
2022-06-29 20:02:15 -0500

    comment

--- customlog.js
+++ customlog.js
@@ -1,5 +1,5 @@
 function customlog(req, res, next) {
-    req.url == "/favicon.ico" ? next() : null;
+    req.url == "/favicon.ico" ? next() : null; // Don't bother if the browser is asking for the favicon
     if (res.headersSent) {
         console.log(`${req.ip}: HTTP ${req.method} ${req.url} (status ${res.statusCode})`)
     } else {
