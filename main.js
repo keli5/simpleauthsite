e5a1cc5e2070f5e915a2e1bf461b1c1a987214d7
keli5 <46039037+keli5@users.noreply.github.com>
2022-06-29 21:58:05 -0500

    uh um

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const PORT = 8080
+const PORT = 27015
 
 
 function makeid(length) {
@@ -15,12 +15,18 @@ const { response } = require("express");
 var express = require("express")
 var app = express()
 var customlog = require("./customlog").customlog
+const cookieParser = require('cookie-parser');
 
 var tokens  = new Map() // map tokens to user info
 var usermap = new Map() // map usernames back to tokens
+var cookieOptions = {   // default options for cookies
+    maxAge: 1000 * 60 * 60 * 24 * 30, // expires after 30d
+    httpOnly: true,
+}
 
 app.use(express.urlencoded({ extended: true }))
 app.use(customlog)
+app.use(cookieParser())
 
 app.get('/home', (req, res) => {
     res.sendFile("home.html", { // send home.html
@@ -37,7 +43,11 @@ app.post('/make_account', (req, res) => { // generate the user token,
     }
     tokens.set(newToken, user)
     usermap.set(user.username, newToken)
-    res.status(200).send(newToken + `<br>Try going to <a href='../get_account/${newToken}'>/get_account/${newToken}</a> now`)
+    res.cookie("Auth-Token", newToken, cookieOptions)
+    sendtext = newToken
+    sendtext += `<br>Try going to <a href='../get_account/${newToken}'>/get_account/${newToken}</a> now`
+    sendtext += `<br>Or go to <a href='../get_account_cookie/'>/get_account_cookie/</a> for cookie auth`
+    res.status(200).send(sendtext)
 })
 
 app.get('/get_account/:token', (req, res) => { 
@@ -47,6 +57,10 @@ app.get('/get_account/:token', (req, res) => {
     } else {                              // else error, token doesn't have information associated
         res.status(404).send({error:"No such token"})
     }
+})
+
+app.get('/get_account_cookie', (req, res) => {
+    res.redirect(`/get_account/${req.cookies['Auth-Token']}`)
 })
 
 app.get("/", (req, res) => { // Redirect / to /home
